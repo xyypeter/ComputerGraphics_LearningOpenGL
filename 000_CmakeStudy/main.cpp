fa441c753a0ba0ba86ec9837bb791333cf1bf90c
钰 刘 <18391128562@163.com>
2026-01-10 22:41:06 +0800

    学习CMake工程管理的基础使用方法

---
+++ 000_CmakeStudy/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "funcs/func.h"
+using namespace std;
+
+int main() {
+    cout << "Hello OpenGL" << endl;
+    cout << add(3,5) << endl;
+    return 0;
+}
---
+++ 000_CmakeStudy/main2.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello OpenGL main2.cpp" << endl;
+    return 0;
+}
