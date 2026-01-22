46547ed2c5e3519d920eca043d713c650eda5990
钰 刘 <18391128562@163.com>
2026-01-22 21:08:24 +0800

    学习GLM库叠加运算 1.连续转动三角形（动画） 2.先平移再叠加旋转 3.先旋转再叠加平移 4.先缩放再叠加平移

---
+++ 025_OpenGL_GLM_SeniorTransform/assets/shaders/vertex.glsl
@@ -0,0 +1,22 @@
+#version 460 core
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aColor;
+layout (location = 2) in vec2 aUV;
+
+out vec3 color;
+out vec2 uv;
+
+uniform mat4 transform;//传入的变换矩阵
+
+
+//aPos作为attribute（属性）传入shader原则是上不允许更改的
+void main()
+{
+	vec4 position = vec4(aPos, 1.0);//将aPos(输入的顶点位置)从vec3转换为vec4在齐次坐标中，四维坐标的第四个分量通常设为 1.0，表示位置坐标
+	position = transform * position;//使用传入的 transform 矩阵对顶点的位置进行变换
+	gl_Position = position;
+	color = aColor;
+	uv = aUV;
+}
+
+
